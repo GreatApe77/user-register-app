9d16472393f3470a85ee910d605e45094542d7af
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-10-20 17:07:25 -0300

    adiciona o morgan

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ require("./config/mongoose-config")
 const express = require("express")
 const { register } = require("./controllers/register")
 const { login } = require("./controllers/login")
+const morgan = require("morgan")
 const PORT = process.env.PORT || 8080
 const app = express()
 
+app.use(morgan("tiny"))
 app.use(express.json())
 
 
