d8ecb37de7b2741c8b422fd9ddd334e797f17bc0
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-10-20 15:47:45 -0300

    cria o model User

---
+++ src/config/mongoose-config.js
@@ -0,0 +1,13 @@
+require("dotenv").config()
+const mongoose = require("mongoose")
+
+mongoose.Promise = global.Promise
+
+mongoose.connect(process.env.MONGOOSE_CONNECTION_STRING,{
+    useNewUrlParser: true,
+	useUnifiedTopology: true,
+}).then(()=>{
+    console.log("Conectou ao mongoose")
+}).catch((err)=>{
+    console.error(err)
+})
